6291e399dbb43a2ec67bad00392b74b73901cf7e
Dilki687 <chamodidilki44@gmail.com>
2025-01-12 23:35:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from './component/Navbar/Navbar';
+import { ThemeProvider } from '@emotion/react';
+import { darkTheme } from './Theme/DarkTheme';
+import { CssBaseline } from '@mui/material';
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
+      <Navbar/>
+    </ThemeProvider>
+  );
+}
+
+export default App;
