8898cb69258956876be1d445a0d892894f3a5529
Dilki687 <chamodidilki44@gmail.com>
2025-01-17 22:54:15 +0530

    Restaurant page

---
+++ src/component/Restaurant/MenuCard.jsx
@@ -0,0 +1,102 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+} from "@mui/material";
+import React from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Category } from "@mui/icons-material";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+
+// const ingredients=[
+//     {
+//         category:"Nuts & Seeds",
+//         ingredients:"Cashews"
+//     },
+//     {
+//         category:"Protein",
+//         ingredients:"Protein"
+//     },
+//     {
+//         category:"Protein",
+//         ingredients:"Bacon Strips"
+
+//     }
+// ]
+
+const demo = [
+  {
+    category: "Nuts & Seeds",
+    ingredients: ["Cashews"],
+  },
+  {
+    category: "Protein",
+    ingredients: ["Ground beef", "Bacon Strips"],
+  },
+];
+
+const MenuCard = () => {
+  const handleCheckBoxChange = (value) => {
+    console.log("value");
+  };
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1-content"
+        id="panel1-header"
+      >
+        <div className="lg:flex items-center justify-between">
+          <div className="lg:flex items-center lg:gap-5">
+            <img
+              className="w-[7rem] h-[7rem] object-cover"
+              src="https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt=""
+            />
+            <div className="space-y-1 lg:space-y-5 lg:max-w-2xl">
+              <p className="font-semibold text-xl">Burger</p>
+              <p>$6</p>
+              <p className="text-gray-400">
+                A juicy, perfectly seasoned patty nestled in a soft, toasted
+                bun, topped with melted cheese, crisp lettuce, ripe tomatoes,
+                and a dollop of tangy sauce—every bite bursts with flavor and
+                satisfaction.
+              </p>
+            </div>
+          </div>
+        </div>
+      </AccordionSummary>
+      <AccordionDetails>
+        <form>
+          <div className="flex gap-5 flex-wrap">
+            {demo.map((item) => (
+              <div>
+                <p>{item.category}</p>
+                <FormGroup>
+                  {item.ingredients.map((item) => (
+                    <FormControlLabel
+                      control={
+                        <Checkbox onChange={() => handleCheckBoxChange(item)} />
+                      }
+                      label={item}
+                    />
+                  ))}
+                </FormGroup>
+              </div>
+            ))}
+          </div>
+          <div className="pt-5">
+            <Button variant="contained" disabled={false} type="submit">
+              {true ? "Add to Cart" : "Out of Stock"}
+            </Button>
+          </div>
+        </form>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default MenuCard;
--- src/component/Restaurant/RestaurantDetails.jsx
+++ src/component/Restaurant/RestaurantDetails.jsx
@@ -1,9 +1,34 @@
-import React from "react";
-import { Grid } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Divider,
+  FormControl,
+  Grid,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import Radio from "@mui/material/Radio";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import MenuCard from "./MenuCard";
+
+const categories = ["pizza", "biriyani", "burger", "chicken", "rice"];
+
+const foodTypes = [
+  { label: "All", value: "all" },
+  { label: "Vegetarian only", value: "vegetarian" },
+  { label: "Non-Vegetarian", value: "non_vegetarian" },
+  { label: "Seasonal", value: "seasonal" },
+];
+
+const menu=[1,1,1,1,1,1]
 
 const RestaurantDetails = () => {
+  const [foodType,setFoodType]=useState("all")
+
+  const handleFilter=(e)=>{
+    console.log(e.target.value,e.target.name)
+  }
   return (
     <div className="px-5 lg:px-20">
       <section>
@@ -52,6 +77,55 @@ const RestaurantDetails = () => {
               <span>Mon-Sun: 9:00 AM - 9:00 PM (Today)</span>
             </p>
           </div>
+        </div>
+      </section>
+      <Divider />
+      <section className="pt-[2rem] lg:flex relative">
+        <div className="space-y-10 lg:w-[20%] filter">
+          <div className="box space-y-5 lg:sticky top-28 d">
+            <div>
+              <Typography variant="h5" sx={{ paddingBottom: "1rem" }}>
+                Food Type
+              </Typography>
+
+              <FormControl className="py-10 space-y-5" component={"fieldset"}>
+                <RadioGroup onChange={handleFilter} name="food_type" value={foodType}>
+                  {foodTypes.map((item) => (
+                    <FormControlLabel
+                    key={item.value}
+                      value={item.value}
+                      control={<Radio />}
+                      label={item.label}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            </div>
+            <Divider/>
+            <div>
+              <Typography variant="h5" sx={{ paddingBottom: "1rem" }}>
+                Food Category
+              </Typography>
+
+              <FormControl className="py-10 space-y-5" component={"fieldset"}>
+                <RadioGroup onChange={handleFilter} name="food_type" value={foodType}>
+                  {categories.map((item) => (
+                    <FormControlLabel
+                    key={item}
+                      value={item}
+                      control={<Radio />}
+                      label={item}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            </div>
+          </div>
+        </div>
+
+        <div className="space-y-5 lg:w-[80%] lg:pl-10">
+          {menu.map((item)=><MenuCard/>)}
+
         </div>
       </section>
     </div>
