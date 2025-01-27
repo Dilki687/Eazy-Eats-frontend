f647dc5d97cd1ff2cb15b0666c80d6ca29007dd5
Dilki687 <chamodidilki44@gmail.com>
2025-01-27 23:10:59 +0530

    payment gateway integration

--- src/Routers/CustomerRoute.jsx
+++ src/Routers/CustomerRoute.jsx
@@ -7,6 +7,7 @@ import Profile from '../component/Profile/Profile'
 import { Navbar } from '../component/Navbar/Navbar'
 import { Auth } from '../component/Auth/Auth'
 import Home from "../component/Home/Home";
+import { PaymentSuccess } from '../component/PaymentSuccess/PaymentSuccess'
 
 export const CustomerRoute = () => {
   return (
@@ -18,6 +19,7 @@ export const CustomerRoute = () => {
             <Route path='/restaurant/:city/:title/:id' element={<RestaurantDetails/>}/>
             <Route path='/cart' element={<Cart/>}/>
             <Route path='/my-profile/*' element={<Profile/>}/>
+            <Route path='/payment/success/:id' element={<PaymentSuccess/>}/>
         </Routes>
         <Auth/>
     </div>
