9805b83daffd7f84bc614fb3e4a1622343461a6a
Dilki687 <chamodidilki44@gmail.com>
2025-01-28 22:30:40 +0530

    creating admin pages

--- src/AdminComponent/Admin/Admin.jsx
+++ src/AdminComponent/Admin/Admin.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import { AdminSideBar } from './AdminSideBar'
+import { Route, Routes } from 'react-router-dom'
+import { Dashboard, RestaurantDashboard } from '../Dashboard/Dashboard'
+import { Orders } from '../Orders/Orders'
+import { Menu } from '../Menu/Menu'
+import { FoodCategory } from '../FoodCategory/FoodCategory'
+import Ingredients from '../Ingredients/Ingredients'
+import { Events } from '../Events/Events'
+import { RestaurantDetails } from './RestaurantDetails'
 
 export const Admin = () => {
     const handleClose=()=>{
@@ -12,6 +20,15 @@ export const Admin = () => {
                 <AdminSideBar handleClose={handleClose}/>
             </div>
             <div className='lg:w-[80%]'>
+              <Routes>
+                <Route path='/' element={<RestaurantDashboard/>}/>
+                <Route path='/orders' element={<Orders/>}/>
+                <Route path='/menu' element={<Menu/>}/>
+                <Route path='/category' element={<FoodCategory/>}/>
+                <Route path='/ingredients' element={<Ingredients/>}/>
+                <Route path='/event' element={<Events/>}/>
+                <Route path='/details' element={<RestaurantDetails/>}/>
+              </Routes>
 
             </div>
 
